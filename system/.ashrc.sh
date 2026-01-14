50206e6d9adea446706b5af1e9ac4fbc29d420dd
cybardev <50134239+cybardev@users.noreply.github.com>
2026-01-14 02:26:26 -0400

    feat: use garage instead of minio for ente

---
+++ system/.ashrc.sh
@@ -0,0 +1,15 @@
+# ~/.ashrc: interactive shell configuration for BusyBox Ash
+
+### Custom Aliases ###
+
+# system
+alias x="exit"
+alias clr="clear"
+alias ls="ls --color=auto"
+alias grep="grep --color=auto"
+alias sudo="doas"
+
+# containers
+alias docker="sudo nerdctl"
+alias garage="docker exec -it ente-s3 /garage"
+alias recompose="docker compose down && docker compose up -d"
