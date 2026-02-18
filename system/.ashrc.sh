3eef7bf979f52fa8939e1e1b55c8886a41695dca
cybardev <50134239+cybardev@users.noreply.github.com>
2026-02-18 02:47:57 -0400

    feat: add alias to connect tailscale services

--- system/.ashrc.sh
+++ system/.ashrc.sh
@@ -12,6 +12,7 @@ alias sudo="doas"
 # containers
 alias docker="sudo nerdctl"
 alias garage="docker exec -it ente-s3 /garage"
+alias tailup="docker exec -it tailscale ts-services.sh"
 alias recompose="docker compose down && docker compose up -d"
 
 # AWS
